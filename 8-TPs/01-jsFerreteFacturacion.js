7927570f828f210dc47fd20f337b85ab08f4e341
emavl <98594410+emavl@users.noreply.github.com>
2022-02-05 20:07:10 -0300

    tp 1 al 3

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -1,17 +1,87 @@
-/*1.	Para el departamento de facturación:
+/*
+Emanuel vidal Div E
+E/S eje ferreteria facturacion 
+.	Para el departamento de facturación:
 A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
-{
+{ //Asigno variables
+	var precioIng1;
+	var PrecioIng2;
+	var precioIng3;
+	var resultado;
 	
+	// traigo los valores
+	precioIng1 = document.getElementById("txtIdPrecioUno").value;
+	PrecioIng2 = document.getElementById("txtIdPrecioDos").value;
+	precioIng3 = document.getElementById("txtIdPrecioTres").value;
+
+	//parseamos
+	precioIng1 = parseFloat(precioIng1);
+	PrecioIng2 = parseFloat(PrecioIng2);
+	precioIng3 = parseFloat(precioIng3);
+
+	//muestro resultado
+	resultado = precioIng1 + PrecioIng2 + precioIng3;
+
+    // Muestro resultado en N°
+	alert(resultado)
+
+
 }
 function Promedio () 
-{
+{ //Asigno variable
+	var precioIng1;
+	var PrecioIng2;
+	var precioIng3;
+	var promedio; 
+
+	// traigo los valores
+	precioIng1 = document.getElementById("txtIdPrecioUno").value;
+	PrecioIng2 = document.getElementById("txtIdPrecioDos").value;
+	precioIng3 = document.getElementById("txtIdPrecioTres").value;
+
+	//parseamos
+	precioIng1 = parseFloat(precioIng1);
+	PrecioIng2 = parseFloat(PrecioIng2);
+	precioIng3 = parseFloat(precioIng3);
+
+	//Muestro promedio
+	promedio = (precioIng1 + PrecioIng2 + precioIng3) /3;
+
+	//muestro promedio
+	alert(promedio);
+
 	
 }
 function PrecioFinal () 
-{
-	
+{	//Asigno variable
+	var precioIng1;
+	var PrecioIng2;
+	var precioIng3;
+	var IVA; 
+	var precioFinal;
+	var resultado
+
+
+	// traigo los valores
+	precioIng1 = document.getElementById("txtIdPrecioUno").value;
+	PrecioIng2 = document.getElementById("txtIdPrecioDos").value;
+	precioIng3 = document.getElementById("txtIdPrecioTres").value;
+
+	//parseamos
+	precioIng1 = parseFloat(precioIng1);
+	PrecioIng2 = parseFloat(PrecioIng2);
+	precioIng3 = parseFloat(precioIng3);
+
+    //sumo el precio y agrego el IVA
+    precioFinal = (precioIng1+PrecioIng2+precioIng3);
+    IVA = (precioFinal*21/100);
+    resultado = (precioFinal+IVA);
+
+    alert("El precio final con el IVA incluido es de : " + resultado );
+
+
 }
--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -6,13 +6,62 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
+	//Asigno para consignas a:
+	var largo;
+	var ancho;
+	var radio;
+    var perimetro;
+    var resultado;
+
+	//Traigo datos
+	largo = document.getElementById("txtIdLargo").value;
+    ancho = document.getElementById("txtIdAncho").value;
+    
+    largo = parseFloat(largo);
+    ancho = parseFloat(ancho);
+    //radio = parseFloat(radio);
+    perimetro = parseFloat(perimetro);
+
+    perimetro =(2*largo + 2*ancho);
+    resultado =(perimetro*3);
+
+
+    alert("la cantidad de alambre para su terreno es de "+ resultado);
+
+
+	// buscar perimetro del rectangulo "formula"
 
 }
 function Circulo () 
 {
+	var radioterreno
+	var resultado
+	var alambre
 	
+	radioterreno = document.getElementById("txtIdRadio").value;
+	radioterreno = parseFloat(radioterreno);
+
+	radioterreno =(2*3.14)*radioterreno;
+	alambre = radioterreno*3;
+
+	alert(" la cantidad de alambre es de: " + alambre + " metros ");
 }
 function Materiales () 
 {
+	var largoterreno
+	var anchoterreno
+    var AreaTerreno
+    
+	largoterreno=document.getElementById('txtIdLargo').value;
+    anchoterreno=document.getElementById('txtIdAncho').value;
+
+    largoterreno=parseFloat(largoterreno);
+    anchoterreno=parseFloat(anchoterreno);
+
+    AreaTerreno=(largoterreno * anchoterreno);
+    bolsaCal=(AreaTerreno * 3);
+    bolsaCemento=(AreaTerreno * 2);
+
+    alert("la cantidad de bolsas necesarias son : " + bolsaCemento + " y la cantidad de bolsas de Cal son " + bolsaCal);
 	
 }
