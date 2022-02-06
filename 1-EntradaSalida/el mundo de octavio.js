cc2ff0eaf071cfe9275587788969ba1b1bcd0680
emavl <98594410+emavl@users.noreply.github.com>
2022-02-06 19:09:35 -0300

    Add files via upload

---
+++ 1-EntradaSalida/el mundo de octavio.js
@@ -0,0 +1,84 @@
+/*
+Emanuel vidal
+ejericicio sabado 6
+el mundo de octavio
+
+AB = Diámetro mayor
+DC = diámetro menor
+BD y BC = lados menores
+AD y AC = lados mayores
+
+Todos los datos se ingresan por prompt. Pueden usar el mismo html del ejercicio 01 de E/S
+
+Debemos tener en cuenta que la estructura del cometa estará dada por un perímetro de varillas de plástico
+ y los correspondientes entrecruces (DC y AB) del mismo material para mantener la forma del cometa.
+
+El cometa estará construido con papel de alta resistencia. La cola del mismo se construirá con
+el mismo papel que el cuerpo
+y representará un 10% adicional del necesario para el cuerpo.
+
+Necesitamos saber cuántos Mts de varillas de plástico y cuántos de papel 
+son necesarios para la construcción en masa de 10 cometas.
+
+Tener en cuenta que los valores de entrada están expresados en Cms.
+
+Ahora necesitamos saber cuánto papel de cada color necesitamos. Las entradas son las mismas.
+*/
+
+function mostrar()
+{ 
+// asigno variable AB = y DC = x
+      var x;
+      var y;
+      var totalVarillas;
+      var totalPapel;
+ 
+      y=prompt("Ingrese diametro mayor y (cm): ");
+      x=prompt("Ingrese diametro menor x (cm): ");
+
+      y=parseInt(y);
+      x=parseInt(x);
+
+// calculo varillas, para esto, calculo el area del cometa (x*y)/2 + x + y:
+     totalVarillas = (((x*y)/2)+x+y)/1000;
+     alert ("El total de Mts de varillas es: "+totalVarillas+" mts.");
+  
+// calculo papel
+     totalPapel = (((x*y)/2)*1.10)/1000;
+     alert ("El total de Mts de papel más el 10% para la cola es: "+totalPapel+" mts.");
+
+
+
+
+
+      //Math.pow, hay indicarle al pow el nro y el exponente: Math.pow(base, exponente)
+      //hipotenusaCorta = Math.sqrt(Math.pow((DC/2),2) + Math.pow((AB/3),2));
+      //hipotenusaLarga = Math.sqrt(Math.pow((DC/2),2) + Math.pow((AB/3),2));
+  
+      
+      
+
+
+
+
+
+}     
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -2,7 +2,7 @@
 Emanuel vidal
 ejericicio sabado 5
 Se piden tres nombre de producto, y los precios de cada producto ingresado,
-sacar el precio bruto(la suma de los tres sin impuestos), el promedio de los precios 
+sacar el precio bruto(la    bruto de los tres sin impuestos), el promedio de los precios 
 y el precio final total más iva(21%), pedir un porcentaje de descuento y aplicarlo al precio final
 , mostrar todos los datos calculados e ingresados por alerta. (solo una alerta en el código). 
 Los datos se piden por prompt.*/
@@ -17,38 +17,45 @@ function mostrar()
     var precio2;
     var precio3;
     //Variables de salida 
-    var suma
-    var promedio
-    var precioFinal
+    var bruto;
+    var promedio;
+    var precioFinalIva;
     //variable entrada
-    var descuento
+    var descuento;
 
-	nombre1 = prompt("ingrese nombre del producto uno");
-	nombre2 = prompt("ingrese nombre del producto dos");
-    nombre3 = prompt("ingrese nombre del producto tres");
-    precio1 = prompt("ingrese precio del producto  "+ nombre1);
-    precio2 = prompt("ingrese precio del producto  "+ nombre2);
-    precio3 = prompt("ingrese precio del producto  "+ nombre3);
+	nombre1 = prompt("ingrese nombre del primer producto: ");
+	nombre2 = prompt("ingrese nombre del segundo producto: ");
+    nombre3 = prompt("ingrese nombre del tercer producto: ");
+    precio1 = prompt("ingrese precio del producto "+ nombre1);
+    precio2 = prompt("ingrese precio del producto "+ nombre2);
+    precio3 = prompt("ingrese precio del producto "+ nombre3);
     
     precio1=parseInt(precio1);
     precio2=parseInt(precio2);
     precio3=parseInt(precio3);
 
-    suma=(precio1+precio2+precio3);
+    bruto=precio1+precio2+precio3;
 
-    promedio=suma/3;
-    precioFinal=suma+21/100;
+    promedio  = bruto/3;
+    precioFinalIva = bruto*1.21;
 
 //tomo el % por prompt
-    porcentaje=prompt("Ingrese descuento");
+    porcentaje=prompt("Ingrese % de descuento: ");
     porcentaje=parseInt(porcentaje);
-//agrego calculos
-    descuento= precioFinal * porcentaje/100;
-    resultado=precioFinal+descuento;
 
-    alert("precio bruto: "+ suma + 
-          "promedio: " + promedio +  
-          "Precio final +IVA:" + precioFinal);
+//agrego calculos
+    descuento= precioFinalIva * (porcentaje/100);
+   
+    resultado=precioFinalIva-descuento;
+    
+    alert("------------------------------------------------------\n"+
+    "El primer producto es "+nombre1+" y su precio es de $ "+precio1+"\n"+
+    "El segundo producto es "+nombre2+" y su precio es de $ "+precio2+"\n"+
+    "El tercer producto es "+nombre3+" y su precio es de $ "+precio3+"\n\n"+
+    "El total bruto es: $ "+  bruto + 
+          "\nEl precio promedio es: $ " + promedio +  
+          "\nEl precio final con el descuento + IVA es: $ " + resultado+"\n"+
+          "------------------------------------------------------");
 
 	
 }
